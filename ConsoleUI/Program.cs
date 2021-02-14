30f76e2a24915f31aa285ca8ed45f36da1c6f03e
beyzasicramazz <beyzasicramazz@gmail.com>
2021-02-14 21:18:07 +0300

    06.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,8 +11,14 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //  ProductTest();
+            //Data Transformation Object - DTO
+            ProductTest();
             //IoC
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -23,9 +29,10 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(50, 100))
+
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
